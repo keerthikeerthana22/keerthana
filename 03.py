874de8c47f96e26d150fff75877ef240af0ba7de
keerthikeerthana22 <52947162+keerthikeerthana22@users.noreply.github.com>
2019-08-10 11:30:27 +0530

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,8 @@
+x=input()
+if(x=='a'or x=='e'or x=='i'or x=='o' or x=='u'or x=='A'or x=='E' or x=='I'or x=='O'or x=='U'):
+	print("vowel")
+elif("A>=x"and "z<=x")or("a.=x" and "z<=x"):
+	print("consonant")
+else:
+	("invalid")
+		
