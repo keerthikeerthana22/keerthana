c69ea042b88c643b3c7a1a1f52a8b8a48481a62f
keerthikeerthana22 <52947162+keerthikeerthana22@users.noreply.github.com>
2019-08-10 11:31:27 +0530

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,8 +1,8 @@
 x=input()
 if(x=='a'or x=='e'or x=='i'or x=='o' or x=='u'or x=='A'or x=='E' or x=='I'or x=='O'or x=='U'):
-	print("vowel")
+	print("Vowel")
 elif("A>=x"and "z<=x")or("a.=x" and "z<=x"):
-	print("consonant")
+	print("Consonant")
 else:
 	("invalid")
 		
